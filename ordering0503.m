bc03da574decfa661e003216f7f09b3e40a26d5e
AdrianHsu <adrianhsu1995@gmail.com>
2017-08-23 18:09:10 +0800

    done ordering

--- ordering0503.m
+++ ordering0503.m
@@ -4,10 +4,19 @@ g = numgrid('S', 8);
 a = delsq(g);
 spy(a);
 a;
-p = symrcm(a); 
-q = amd(a); %approximate minimum degree
+p = symrcm(a);
+% spy(p);
+% q = amd(a); %approximate minimum degree
 a2 = a(p, p); 
 r = chol(a);
 r2 = chol(a2);
-nnz(r);
 res = nnz(r);
+res2 = nnz(r2);
+
+% compare RCM result
+[i,j] = find(a);
+bw = max(i-j) + 1;
+% after RCM
+[i2, j2] = find(p);
+bw2 = max(i2-j2) + 1;
+% bw2 should be less than bw1
