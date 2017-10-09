12a1f8514c6cda8994e7afd69d98e9fa4dc014f4
AdrianHsu <adrianhsu1995@gmail.com>
2017-10-10 01:19:34 +0800

    create ipm

--- hw1/ipm.m
+++ hw1/ipm.m
@@ -1,12 +1,12 @@
-function [mu, x] = invpmethod(A, x, tol, N)
+function [mu, x] = ipm(A, x, tol, N)
     q = (x'*A*x) / (x'*x);
     k = 1;
     n = length(x);
+    
     [~, index] = max(abs(x));
     
     x = x./x(index);
-    fileID = fopen('invpmethod-d.txt','w');
-    fprintf(fileID,'%6s %12s %s \n','k','mu', 'x');
+    
     while k <= N
         % solve for (A-qI)y = x
         y = (A-q*eye(n))\(x);
@@ -21,8 +21,6 @@ function [mu, x] = invpmethod(A, x, tol, N)
             mu = (1/mu) + q;
             break;
         end
-        fprintf(fileID, '%6.0f %12.4f (%s)\n',k, mu, strjoin(cellstr(num2str(x(:))),', '));
         k = k + 1;
     end
-    fclose(fileID);
 end
